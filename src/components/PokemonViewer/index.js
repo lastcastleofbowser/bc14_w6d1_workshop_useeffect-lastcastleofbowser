be3a3077d0be5411f1e655f8cf685a8422795625
lastcastleofbowser <123087687+lastcastleofbowser@users.noreply.github.com>
2023-04-17 17:13:14 +0100

    some of task 1 done. Id wouldnt work and I don't know why

--- src/components/PokemonViewer/index.js
+++ src/components/PokemonViewer/index.js
@@ -1,8 +1,35 @@
+import { useState, useEffect } from "react";
+
 function PokemonViewer({ id }) {
+  const [name, setName] = useState("")
+  const [weight, setWeight] =useState ("")
+  const [pokemonId, setPokemonId] =useState ("")
+
   //TODO: Task 1 - send http request to `https://pokeapi.co/api/v2/pokemon/1' and display the data
+  useEffect (()=> {
+async function fetchPokemon(){
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/1", {
+    headers: { accept: "application/json"},
+  });
+  const data = await res.json();
+  console.log(data);
+  setName(data.name)
+  setWeight(data.weight)
+  setPokemonId(data.id)
+}
+fetchPokemon();
+  }, []);
+  
   // TODO: Task 2 - send http request to `https://pokeapi.co/api/v2/pokemon/${id}` and display the data!
   // HINT: you will need useState and useEffect!
-  return <div className="pokemon-viewer"></div>;
+
+
+
+  return <div className="pokemon-viewer">
+  <p>Name: {name}</p>
+  <p>Weight: {weight}kg</p>
+  <p>Pokemon ID: {id}</p>
+  </div>;
 }
 
 export default PokemonViewer;
